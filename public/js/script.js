8a0873384a0c2f72e5f570265bd6815a82d7a4ef
coder <coder@exapmple.com>
2023-11-02 17:20:54 -0700

    commit

---
+++ public/js/script.js
@@ -0,0 +1,108 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const allButtons = document.querySelectorAll(".searchBtn");
+    const searchBar = document.querySelector(".searchBar");
+    const searchInput = document.getElementById("searchInput");
+    const searchClose = document.getElementById("searchClose");
+
+    for (var i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener("click", function () {
+            searchBar.style.visibility = "visible";
+            searchBar.classList.add("open");
+            this.setAttribute("aria-expanded", "true");
+        });
+    }
+
+})
+
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropdownMenu = document.querySelector(".dropdown_menu");
+
+toggleBtn.addEventListener("click", () => {
+    dropdownMenu.classList.toggle("open")
+    const isOpen = dropdownMenu.classList.contains("open");
+
+    toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
+})
+
+var slider_img = document.querySelector(".slider_img");
+var images = ["book lover.svg", "mobile.svg", "reading.svg"];
+var i = 0; 
+
+function prev() {
+    document.querySelector("#word").textContent = "Comfort";
+    document.querySelector("#wordd").textContent = "Comfort";
+    document.querySelector("#word-el").textContent = "Get curl up with a good book, and get lost in the pages of a captivating story. Escape the stresses of everyday life with a good book.";
+    if (i <= 0) i = images.length;
+    i--;
+    return setImg(); 
+}
+
+function next() {
+    document.querySelector("#word").textContent = "Delight";
+    document.querySelector("#wordd").textContent = "Delight";
+    document.querySelector("#word-el").textContent = "Discovering the latest release, explore your favorite genres, and find your next favorite author on bookwave.";
+    if (i >= images.length - 1) i = -1;
+    i++;
+    return setImg(); 
+}  
+
+function setImg() {
+    return slider_img.setAttribute("src", "img/" + images[i]);
+}
+
+document.querySelector("#prev").addEventListener("click", prev);
+document.querySelector("#next").addEventListener("click", next); 
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle("active");
+
+        var pannel = this.nextElementSibling;
+        if(pannel.style.display === "block") {
+            pannel.style.display = "none";
+        } else {
+            pannel.style.display = "block";
+        }
+    });
+}
+
+const accord = document.getElementById("accord");
+accord.addEventListener("click", function() {
+    const icon = document.querySelector("#icon-1");
+        icon.classList.toggle("bi-chevron-up");
+})
+
+const accord2 = document.getElementById("accord-2");
+accord2.addEventListener("click", function() {
+    const icon2 = document.querySelector("#icon-2");
+        icon2.classList.toggle("bi-chevron-up");
+})
+
+const accord3 = document.getElementById("accord-3");
+accord3.addEventListener("click", function() {
+    const icon3 = document.querySelector("#icon-3");
+        icon3.classList.toggle("bi-chevron-up");
+})
+
+const accord4 = document.getElementById("accord-4");
+accord4.addEventListener("click", function() {
+    const icon4 = document.querySelector("#icon-4");
+        icon4.classList.toggle("bi-chevron-up");
+})
+
---
+++ server/routes/main.js
@@ -0,0 +1,252 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+const Auth = require("../models/Auth");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const welcomeLayout = "../views/layouts/welcome";
+
+
+
+
+// handle errors
+const handleErrors = (err) => { 
+    console.log(err.message, err.code);
+    let errors = { email: "", password: ""};
+    
+    // incorrect email message
+    if(err.message === "Incorrect email") {
+        errors.email = "that email is not registered"; 
+    }
+    
+    // incorrect password message
+    if(err.message === "Incorrect password") {
+        errors.password = "that password is not valid";
+    }
+
+    // duplicate error code 
+    if(err.code === 11000) {
+        errors.email = "email is already registered";
+        return errors;  
+    } 
+
+    // validation errors
+    if (err.message.includes("Auth validation failed")) {
+        Object.values(err.errors).forEach(({properties}) => {
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
+
+const requireAuth = (req,res, next) => { 
+    const token = req.cookies.jwt;
+
+    // check json web token exists & is verified
+    if (token) {
+        jwt.verify(token, "hive point secret", (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.redirect("/login")
+            } else {
+                console.log(decodedToken);
+                next();
+            }
+        })
+    } else {
+        res.redirect("/login");
+    }
+}
+
+// routes
+router.get("/", (req,res) => {
+   res.render("home",  { 
+    title: "Book Wave", currentRoute: "/home", layout: welcomeLayout })
+});
+
+router.get("/story", requireAuth, async (req,res) => {
+    try {
+        const locals = {
+            title: "Love and Justice",
+            description: "Simple Blog created with NodeJs, Express & MongoDb."
+        }
+
+        let perPage = 7;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { updatedAt: 1 } }])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+
+        res.render("story", {
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: "/story"
+            });
+    } catch(error) {
+        console.log(error);
+    }
+});
+
+
+router.get( "/post/:id", async (req, res) => {
+    try {
+    
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+
+    const locals = {
+        title: data.title,
+        description: "Simple Blog ceated with NodeJs, Express & MongoDB",
+        currentRoute: `/post/${slug}`
+    }
+    res.render("post", { locals, data, currentRoute: `/post/${slug}` });
+    } catch (error) {
+        console.log(error);
+    }
+});
+  
+
+
+  
+
+
+router.post( "/search", async (req, res) => {
+    try {
+    const locals = {
+        title: "Search",
+        description: "Simple Blog ceated with NodeJs, Express & MongoDB",
+    }
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-z0-9]/g, "")
+
+    const data = await Post.find({
+        $or : [
+            { title: { $regex: new RegExp(searchNoSpecialChar, "i") }},
+            { body: { $regex: new RegExp(searchNoSpecialChar, "i") }}  
+        ]
+    });
+
+    res.render("search", {
+        data,
+        locals,
+        currentRoute: `/post/${searchNoSpecialChar}`
+    });
+
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+  
+router.get("/page", requireAuth, (req,res) => {
+    res.render("page", {
+        currentRoute: "/page", title: "Home"
+    });
+});
+
+
+
+const maxAge = 3 * 24 * 60 *60;
+const createToken = (id) => {
+    return jwt.sign({ id }, "hive point secret", {
+        expiresIn: maxAge
+    });
+}
+
+router.get("/signup", (req, res) => {
+    res.render("signup",  { 
+        title: "Sign Up", currentRoute: "/signup", layout: welcomeLayout });
+});
+
+router.post("/signup", async (req, res) => {
+    const { email, password } = req.body;
+    
+    try {
+       const user = await Auth.create({ email, password });
+       const token = createToken(user._id);
+       res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+       res.status(201).json({ user: user._id }); 
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors});
+    }
+});
+
+
+router.get("/login", (req, res) => {
+    res.render("login", { 
+        title: "Login", currentRoute: "/login", layout: welcomeLayout });
+});
+
+router.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await Auth.findOne({ email });
+        if (user) { 
+            const auth = await bcrypt.compare(password, user.password); 
+            if (auth) {
+                const token = createToken(user._id);
+                res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+                res.status(201).json({ user: user._id });
+                return user;
+            } 
+            throw Error("Incorrect password"); 
+        }
+        throw Error("Incorrect email");
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+});
+
+router.get("/logout", (req,res) => {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.redirect("/");
+})
+
+router.get("/construct", (req, res) => {
+    res.render("construct", { 
+        title: "Construction", currentRoute: "/construct" });
+});
+
+router.get("/building", (req, res) => {
+    res.render("building", { 
+        title: "Under Uploading", currentRoute: "/building" });
+});
+
+router.get("/admin", (req, res) => {
+    res.render("admin/index", { 
+        title: "Admin Login", currentRoute: "admin/index" });
+});
+
+// cookies
+router.get("/set-cookies", (req,res) => {
+    // res.setHeader("Set-Cookie", "newUser=true");
+    res.cookie("newUser", false);
+    res.cookie("isEmployee", true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true });
+
+    res.send("you got the cookies!");
+})
+
+router.get("/read-cookies", (req,res) => {
+    const cookies = req.cookies;
+    console.log(cookies.newUser);
+    res.json(cookies);
+})
+
+module.exports = router;
